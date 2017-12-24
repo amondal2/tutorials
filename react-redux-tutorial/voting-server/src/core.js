5e8c1ff5f300e072680120f9bf080c77138b7daf
Agastya Mondal <amondal2@github.com>
2017-12-23 23:47:46 -0500

    Adding voting server code

---
+++ react-redux-tutorial/voting-server/src/core.js
@@ -0,0 +1,21 @@
+import {List, Map} from 'immutable';
+
+export function setEntries(state, entries) {
+    return state.set('entries', List(entries));
+}
+
+export function next(state) {
+    const entries = state.get('entries');
+    return state.merge({
+        vote: Map({pair: entries.take(2)}),
+        entries: entries.skip(2)
+    });
+}
+
+export function vote(state, entry) {
+    return state.updateIn(
+        ['vote', 'tally', entry],
+        0,
+        tally => tally + 1
+    );
+}
