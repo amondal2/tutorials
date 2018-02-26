fe4b85b6c498dad2068cfafba6df63ca243637e1
Agastya Mondal <amondal2@github.com>
2018-02-25 21:16:25 -0500

    initial setup for grapql server for slack clone

---
+++ slack-clone/slack-clone-server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+
+import typeDefs from './schema';
+import resolvers from './resolvers';
+
+export const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+});
+const app = express();
+const graphqlEndpoint = '/graphql'
+// bodyParser is needed just for POST.
+app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
+
+app.listen(8080);
