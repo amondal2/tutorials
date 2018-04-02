cadee2f807fc6e1f79786a19a45eb5431767734d
Agastya Mondal <amondal13@gmail.com>
2018-04-01 21:34:53 -0400

    1.2 Is Permutation

---
+++ ctci/chapter1/IsPermutation.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class IsPermutation {
+    public static String sort(String s) {
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+
+    public static boolean isPermutation(String a, String b) {
+        return sort(a) == sort(b);
+    }
+
+    public static void main(String[] args) {
+        assert isPermutation("aba", "baa") == true;
+        assert isPermutation("abc", "ccc") == false;
+        assert isPermutation("gus", "sug") == true;
+
+    }
+}
