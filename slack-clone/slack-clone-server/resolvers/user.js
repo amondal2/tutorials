40d80645fc5def7371717aeddc4e66a28e7b1b14
Agastya Mondal <amondal13@gmail.com>
2018-04-08 14:26:21 -0400

    Adding password hashing to user register resolver

--- slack-clone/slack-clone-server/resolvers/user.js
+++ slack-clone/slack-clone-server/resolvers/user.js
@@ -1,9 +1,20 @@
+import Hashes from 'jshashes';
+
 export default {
   Query: {
-    getUser: (parent, { id }, { models }) => models.User.findOne({ where: { id } }),
+    getUser: (parent, { id }, { models }) =>
+      models.User.findOne({ where: { id } }),
     allUsers: (parent, args, { models }) => models.User.findAll(),
   },
   Mutation: {
-    createUser: (parent, args, { models }) => models.User.create(args),
+    register: async (parent, { password, ...otherArgs }, { models }) => {
+      try {
+        const hashedPassword = new Hashes.SHA1().b64(password);
+        await models.User.create({ ...otherArgs, password: hashedPassword });
+        return true;
+      } catch (err) {
+        return false;
+      }
+    },
   },
 };
