8455bcdb10337d4df740314bb0d8c9995aa54632
Agastya Mondal <amondal13@gmail.com>
2018-04-17 22:22:51 -0400

    adding route to index

--- slack-clone/slack-clone-client/src/routes/index.js
+++ slack-clone/slack-clone-client/src/routes/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import decode from 'jwt-decode';
 
 import Home from './Home';
 import Register from './Register';
 import Login from './Login';
 import CreateTeam from './CreateTeam';
-import decode from 'jwt-decode';
+import ViewTeam from './ViewTeam';
 
 const isAuthenticated = () => {
   const token = localStorage.getItem('token');
@@ -41,6 +42,7 @@ export default () => (
       <Route path="/" exact component={Home} />
       <Route path="/register" exact component={Register} />
       <Route path="/login" exact component={Login} />
+      <Route path="/view-team" exact component={ViewTeam} />
       <PrivateRoute path="/create-team" exact component={CreateTeam} />
     </Switch>
   </BrowserRouter>
