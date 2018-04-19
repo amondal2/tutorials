c7f498768db09fabbe0cbec6d6d694be089ebf3a
Agastya Mondal <amondal13@gmail.com>
2018-04-18 21:55:30 -0400

    adding props to sidebar components

--- slack-clone/slack-clone-client/src/components/Teams.js
+++ slack-clone/slack-clone-client/src/components/Teams.js
@@ -1,7 +1,21 @@
 import styled from 'styled-components';
+import React from 'react';
 
-export default styled.div`
+const TeamWrapper = styled.div`
   grid-column: 1;
   grid-row: 1 / 4;
   background-color: #362234;
+  color: #958993;
 `;
+
+const team = ({ id, letter }) => <li key={`team-${id}`}>{letter}</li>;
+
+export default ({ teams }) => (
+  <TeamWrapper>
+    <div>
+      <ul>
+        {teams.map(team)}
+      </ul>
+    </div>
+  </TeamWrapper>
+);
