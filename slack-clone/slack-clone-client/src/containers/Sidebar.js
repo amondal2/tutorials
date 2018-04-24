76a9cf5e9042f8eed505ffd8c48db8024cccf032
Agastya Mondal <amondal13@gmail.com>
2018-04-23 21:26:04 -0400

    adding containers

---
+++ slack-clone/slack-clone-client/src/containers/Sidebar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import findIndex from 'lodash/findIndex';
+import decode from 'jwt-decode';
+
+import Channels from '../components/Channels';
+import Teams from '../components/Teams';
+
+const Sidebar = ({ data: { loading, allTeams }, currentTeamId }) => {
+  if (loading) {
+    return null;
+  }
+
+  const teamIdx = findIndex(allTeams, ['id', currentTeamId]);
+  const team = allTeams[teamIdx];
+  let username = '';
+  try {
+    const token = localStorage.getItem('token');
+    const { user } = decode(token);
+    // eslint-disable-next-line prefer-destructuring
+    username = user.username;
+  } catch (err) {}
+  return [
+    <Teams
+      key="team-sidebar"
+      teams={allTeams.map(t => ({
+        id: t.id,
+        letter: t.name.charAt(0).toUpperCase(),
+      }))}
+    />,
+    <Channels
+      key="channels-sidebar"
+      teamName={team.name}
+      username={username}
+      channels={team.channels}
+      users={[{ id: 1, name: 'Slackbot' }, { id: 2, name: 'user1' }]}
+    />,
+  ];
+};
+
+const allTeamsQuery = gql`
+  {
+    allTeams {
+      id
+      channels {
+        id
+        public
+        name
+      }
+      name
+    }
+  }
+`;
+
+export default graphql(allTeamsQuery)(Sidebar);
