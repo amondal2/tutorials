5d3b801fa32c19a6f29cd2b02d1b038391429851
Agastya Mondal <amondal13@gmail.com>
2018-04-23 23:11:22 -0400

    update eslint config

--- slack-clone/.eslintrc.js
+++ slack-clone/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
         'react/jsx-filename-extension': 0,
         'arrow-parens': 0,
         'react/prop-types': 0,
+        'jsx-a11y/anchor-is-valid': 0,
     },
     "globals": {
         "document": 1
