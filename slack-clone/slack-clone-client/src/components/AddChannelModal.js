b31149570312f4436d74102797abd5e4a6d1641b
Agastya Mondal <amondal13@gmail.com>
2018-04-25 21:56:18 -0400

    adding addchannelmodal with graphql request

---
+++ slack-clone/slack-clone-client/src/components/AddChannelModal.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Form, Input, Button, Header, Modal } from 'semantic-ui-react';
+import { withFormik } from 'formik';
+import gql from 'graphql-tag';
+import { compose, graphql } from 'react-apollo';
+
+const AddChannelModal = ({
+  open,
+  onClose,
+  values,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  isSubmitting,
+}) => (
+  <Modal open={open} onClose={onClose}>
+    <Header>Add Channel</Header>
+    <Modal.Content>
+      <Form>
+        <Form.Field>
+          <Input
+            value={values.name}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            name="name"
+            fluid
+            placeholder="Channel name"
+          />
+        </Form.Field>
+        <Form.Group widths="equal">
+          <Button disabled={isSubmitting} onClick={handleSubmit} fluid>
+            {' '}
+            Create Channel{' '}
+          </Button>
+          <Button disabled={isSubmitting} fluid onClick={onClose}>
+            {' '}
+            Cancel{' '}
+          </Button>
+        </Form.Group>
+      </Form>
+    </Modal.Content>
+  </Modal>
+);
+
+const createChannelMutation = gql`
+  mutation($teamId: Int!, $name: String!) {
+    createChannel(teamId: $teamId, name: $name)
+  }
+`;
+
+export default compose(
+  graphql(createChannelMutation),
+  withFormik({
+    mapPropsToValues: () => ({ name: '' }),
+    handleSubmit: async (
+      values,
+      { props: { onClose, teamId, mutate }, setSubmitting },
+    ) => {
+      await mutate({
+        variables: { teamId, name: values.name },
+      });
+      onClose();
+      setSubmitting(false);
+    },
+  }),
+)(AddChannelModal);
--- slack-clone/slack-clone-client/src/components/Channels.js
+++ slack-clone/slack-clone-client/src/components/Channels.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import React from 'react';
+import { Icon } from 'semantic-ui-react';
 
 const ChannelWrapper = styled.div`
   grid-column: 2;
@@ -45,7 +46,7 @@ const user = ({ id, name }) => (
   </SideBarListItem>
 );
 
-export default ({ teamName, username, channels, users }) => (
+export default ({ teamName, username, channels, users, onAddChannelClick }) => (
   <ChannelWrapper>
     <PushLeft>
       <TeamNameHeader>{teamName}</TeamNameHeader>
@@ -53,7 +54,9 @@ export default ({ teamName, username, channels, users }) => (
     </PushLeft>
     <div>
       <SideBarList>
-        <SideBarListHeader>Channels</SideBarListHeader>
+        <SideBarListHeader>
+          Channels<Icon onClick={onAddChannelClick} name="add circle" />
+        </SideBarListHeader>
         {channels.map(channel)}
       </SideBarList>
     </div>
