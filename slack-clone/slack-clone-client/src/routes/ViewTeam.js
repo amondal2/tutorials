11717ca23f312651540366f75a6a29bd9a3fd9f4
Agastya Mondal <amondal13@gmail.com>
2018-04-29 17:55:53 -0400

    checking team / channel exists and redirecting otherwise

--- slack-clone/slack-clone-client/src/routes/ViewTeam.js
+++ slack-clone/slack-clone-client/src/routes/ViewTeam.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import findIndex from 'lodash/findIndex';
+import { Redirect } from 'react-router-dom';
 
 import { allTeamsQuery } from '../graphql/team';
 import Header from '../components/Header';
@@ -17,11 +18,21 @@ const ViewTeam = ({
     return null;
   }
 
-  const teamIdx = teamId
-    ? findIndex(allTeams, ['id', parseInt(teamId, 10)])
+  if (!allTeams.length) {
+    return <Redirect to="/create-team" />;
+  }
+
+  const teamIdInteger = parseInt(teamId, 10);
+
+  const teamIdx = teamIdInteger
+    ? findIndex(allTeams, ['id', teamIdInteger])
     : 0;
   const team = allTeams[teamIdx];
-  const channelIdx = channelId ? findIndex(team.channels, ['id', parseInt(channelId, 10)]) : 0;
+
+  const channelIdInteger = parseInt(channelId, 10);
+  const channelIdx = channelIdInteger
+    ? findIndex(team.channels, ['id', channelIdInteger])
+    : 0;
   const channel = team.channels[channelIdx];
 
   return (
@@ -33,14 +44,16 @@ const ViewTeam = ({
         }))}
         team={team}
       />
-      <Header channelName={channel.name} />
-      <Messages>
-        <ul className="message-list">
-          <li />
-          <li />
-        </ul>
-      </Messages>
-      <SendMessage channelName={channel.name} />
+      {channel && <Header channelName={channel.name} />}
+      {channel && (
+        <Messages>
+          <ul className="message-list">
+            <li />
+            <li />
+          </ul>
+        </Messages>
+      )}
+      {channel && <SendMessage channelName={channel.name} />}
     </AppLayout>
   );
 };
