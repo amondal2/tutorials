a977eda6c0460ab01bc66ed219c0ad1a2d6affc7
Agastya Mondal <amondal13@gmail.com>
2018-04-29 14:41:03 -0400

    normalize errors util

---
+++ slack-clone/slack-clone-client/src/normalizeErrors.js
@@ -0,0 +1,16 @@
+// input: [{path: '', message:''}]
+/* 
+{
+    email: ['e1', 'e2, ...]
+}
+*/
+
+export default errors =>
+  errors.reduce((acc, cv) => {
+    if (cv.path in acc) {
+      acc[cv.path].push(cv.message);
+    } else {
+      acc[cv.path] = [cv.message];
+    }
+    return acc;
+  }, {});
