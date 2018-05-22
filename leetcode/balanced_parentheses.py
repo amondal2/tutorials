e018932ed4489c63f1257dae2d195611cf3087ee
Agastya Mondal <amondal13@gmail.com>
2018-05-22 09:30:42 -0400

    Update balanced_parentheses.py

--- leetcode/balanced_parentheses.py
+++ leetcode/balanced_parentheses.py
@@ -3,7 +3,6 @@ closing = (")", "]", "}")
 mapping = dict(zip(opening, closing))
 def is_balanced(s):
     parens_stack = []
-    parens_stack = []
     for i in range(len(s)):
         if s[i] in opening:
             parens_stack.append(s[i])
