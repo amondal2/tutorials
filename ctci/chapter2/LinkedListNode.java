aab0c50092150d91af116b2c88a408ac84b8384c
Agastya Mondal <amondal13@gmail.com>
2018-06-23 10:15:01 -0400

    adding linkedlistnode class

---
+++ ctci/chapter2/LinkedListNode.java
@@ -0,0 +1,43 @@
+// LinkedListNode utility class used by CTCI book
+
+public class LinkedListNode {
+    public LinkedListNode next, prev, last;
+    public int data;
+    public LinkedListNode(int d, LinkedListNode n, LinkedListNode p) {
+        data = d;
+        setNext(n);
+        setPrevious(p);
+    }
+
+    public LinkedListNode(int d) {
+        data = d;
+    }
+
+    public LinkedListNode() {}
+
+    public void setNext(LinkedListNode n) {
+        next = n;
+        if (this == last) {
+            last = n;
+        }
+        if ( n != null && n.prev != this) {
+            n.setPrevious(this);
+        }
+    }
+
+    public void setPrevious(LinkedListNode p) {
+        prev = p;
+        if (p != null && p.next != this) {
+            p.setNext(this);
+        }
+    }
+
+    public LinkedListNode clone() {
+        LinkedListNode next2 = null;
+        if (next != null) {
+            next2 = next.clone();
+        }
+        LinkedListNode head2 = new LinkedListNode(data, next2, null);
+        return head2;
+    }
+}
