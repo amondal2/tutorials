899059d0df1e221b0d63f84b81263400c3e00ac0
Agastya Mondal <amondal13@gmail.com>
2018-07-30 19:51:29 -0400

    fizzbuzz lol

---
+++ leetcode/fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz():
+    for i in range(1,100):
+        if i % 5 == 0 and i % 3 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+if __name__ == "__main__":
+    fizzbuzz()
