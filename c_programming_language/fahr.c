99189d60e6d9a824402745f8559accdb9aaca882
Agastya Mondal <amondal13@gmail.com>
2018-09-23 13:16:33 -0400

    add header

--- c_programming_language/fahr.c
+++ c_programming_language/fahr.c
@@ -10,6 +10,7 @@ main() {
     step = 20;
 
     fahr = lower;
+    printf("Fahrenheit \t Celsius \n");
     while (fahr <= upper) {
         celsius = (5.0/9.0)  * (fahr-32.0);
         printf("%3.0f %6.1f\n", fahr, celsius);
