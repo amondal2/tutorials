df58e329610a439feb4c9910bd0177476a8b84c3
Agastya Mondal <amondal13@gmail.com>
2018-09-23 13:19:28 -0400

    replace magic numbers with constants

--- c_programming_language/fahr.c
+++ c_programming_language/fahr.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
-
+#define LOWER 0
+#define UPPER 300
+#define STEP 20 
 /* print Fahrenheit-Celsius table */
 
 main() {
     float fahr, celsius;
-    int lower, upper, step;
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
+    fahr = LOWER;
     printf("Fahrenheit \t Celsius \n");
-    while (fahr <= upper) {
+    while (fahr <= UPPER) {
         celsius = (5.0/9.0)  * (fahr-32.0);
         printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+        fahr = fahr + STEP;
     }
 }
